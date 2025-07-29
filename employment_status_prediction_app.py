603c6deb6b97e6f701813fee8aa3daaea5b03e2b
John Antwi-Nuamah Jr <andre061933@gmail.com>
2025-07-29 16:37:12 +0000

    employment_status_prediction_app.py

--- employment_status_prediction_app.py
+++ employment_status_prediction_app.py
@@ -115,8 +115,12 @@ elif selected == "Predictor":
             input_dict[col] = st.selectbox("Gender", ['Female', 'Male'])
         elif col == 'Highest_Education':
             input_dict[col] = st.selectbox("Highest Education", ['Degree', 'Diploma', 'Matric', 'None'])
-        elif col in ['Math', 'Mathlit', 'Additional lang', 'Home lang', 'Science', 'Geography', 'Province', 'Status']:
+        elif col in ['Math', 'Mathlit', 'Additional_lang', 'Home_lang', 'Science', 'Geography']:
             input_dict[col] = st.selectbox(col, ['0% - 49%', '50% - 79%','80% - 100%'])
+        elif col in ['Province']:
+            input_dict[col] = st.selectbox(col, ['Gauteng' , 'Mpumalanga' , 'North West', 'Free State', 'Eastern Cape', 'Limpopo','KwaZulu-Natal','Northern Cape','Western Cape'])
+        elif col in ['Status']:
+            input_dict[col] = st.selectbox(col, ['Unemployed', 'Studying','Wage employed','Self employed','Wage and self employed','Employment program','Other'])
         else:
             input_dict[col] = st.number_input(col, value=0)
 
