04783f7d01f601b8927711b30087e3bc956d5582
John Antwi-Nuamah Jr <andre061933@gmail.com>
2025-08-09 12:37:06 +0000

    employment_status_prediction_app.py

--- employment_status_prediction_app.py
+++ employment_status_prediction_app.py
@@ -117,8 +117,10 @@ elif selected == "Predictor":
             input_dict[col] = st.selectbox("Gender", ['Female', 'Male'])
         elif col == 'Highest_Education':
             input_dict[col] = st.selectbox("Highest Education", ['Degree', 'Diploma', 'Matric', 'None'])
-        elif col in ['Math', 'Mathlit', 'Additional_lang', 'Home_lang', 'Science', 'Geography']:
+        elif col in ['Math', 'Mathlit', 'Additional_lang', 'Home_lang', 'Science']:
             input_dict[col] = st.selectbox(col, ['0% - 49%', '50% - 79%','80% - 100%'])
+        elif col == 'Geography':
+            input_dict[col] = st.selectbox(col, ['Urban','Rural','Suburb'])
         elif col in ['Province']:
             input_dict[col] = st.selectbox(col, ['Gauteng' , 'Mpumalanga' , 'North West', 'Free State', 'Eastern Cape', 'Limpopo','KwaZulu-Natal','Northern Cape','Western Cape'])
         elif col in ['Status']:
